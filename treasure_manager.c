96366beeacc2535266c666d5ad56dab31f704ed7
Rosca Albert <roscaalbert123@gmail.com>
2025-04-12 22:54:48 +0300

    Sper sa fie gata

--- treasure_manager.c
+++ treasure_manager.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #define USERNAME_SIZE 32
 #define CLUE_SIZE 128
@@ -25,10 +29,9 @@ void log_opperation(const char *hunt_id,const char *message){
     char path[FILENAME_SIZE];
     snprintf(path,sizeof(path),"%s,%s",hunt_id,FILE_INPUT);
 
-    int file = open(path, O_WRONLY | O_CREAT | O_APPEND);
+    int file = open(path, O_WRONLY | O_CREAT | O_APPEND, 0644);
 
     if(file < 0 ){
-
       perror("eroare la deschiderea fisierlui la logare\n");
       return;
     }
@@ -47,23 +50,23 @@ void log_opperation(const char *hunt_id,const char *message){
  
 void adauga_comoara(const char *file_input, const char *hunt_id){
 
+  mkdir(hunt_id, 0755);
+
   char file_path[FILENAME_SIZE];
   snprintf(file_path,sizeof(file_path),"%s,%s",hunt_id,"treasures.bin");
 
-  int file = open(file_path, O_WRONLY | O_CREAT | O_APPEND);
+  int file = open(file_path, O_WRONLY | O_CREAT | O_APPEND, 0644);
 
-    if(file < 0 ){
-
+    if(file < 0){
       perror("eroare la deschiderea fisierlui la adaugare \n");
       return;
     }
 
-    FILE *f_input = fopen(file_input,"r");
-    if(f_input){
-
-      perror("eroare de la deschiderea fisierului de input");
-      close(file);
-      return;
+    FILE *f_input = fopen(file_input, "r");
+    if (!f_input) {
+        perror("fopen input file");
+        close(file);
+        return;
     }
 
     Treasure t;
@@ -94,17 +97,17 @@ void lista_treasure(const char *hunt_id){
   int file = open(f_input, O_RDONLY);
 
   if(file < 0 ){
-
     perror("eroare la deschiderea fisierlui la listare\n");
     return;
   } 
-  /*
+
+
   struct stat st;
   if(stat(f_input,&st) == 0){
   printf("Hunt : %s\n", hunt_id);
-  printf("File size : %ld \n", &st.st_size);
-  }*/
-  printf("Hunt : %s\n", hunt_id);
+  printf("File size : %lld \n", st.st_size);
+  printf("Last modified : %s", ctime(&st.st_mtime));
+  }
 
   Treasure t;
   while (read(file,&t,sizeof(Treasure)) == sizeof(Treasure))
@@ -124,7 +127,6 @@ void view_treasure(const char *hunt_id,int id){
   int file = open(f_input, O_RDONLY);
 
   if(file < 0 ){
-
     perror("eroare la deschiderea fisierlui la view\n");
     return;
   }  
@@ -134,16 +136,77 @@ void view_treasure(const char *hunt_id,int id){
   { 
     if(t.treasure_id == id){
     printf("ID : %d, User : %s, Lat : %.2f, Lon : %.2f, Value : %d, Clue : %s \n", t.treasure_id, t.username, t.latitude, t.longitude, t.value, t.clue);
-    close(file);
+
     char msg[128];
     snprintf(msg,sizeof(msg), "Treasure gaist %d", id);
     log_opperation(hunt_id,msg);
+    close(file);
     return;
     }
   } 
 
   printf("Id-ul nu a fost gasit \n");
   close(file);
+}
+
+void sterge_treasure(const char *hunt_id, int id){
+  char f_input[FILENAME_SIZE];
+  snprintf(f_input,sizeof(f_input),"%s,%s",hunt_id,"treasures.bin");
+
+  int file = open(f_input, O_RDONLY);
+  if (file < 0){
+    perror("Eroare la deschiderea fisierului de stergere");
+    return;
+  }
+
+  int f_temp = open("temp.bin", O_WRONLY| O_CREAT | O_TRUNC, 0644);
+  if(f_temp < 0){
+    perror("Eroare la deschiderea fisierului temporar in stergere");
+    close(file);
+    return;
+  }
+
+  Treasure t;
+  int found = 0;
+  while (read(file,&t,sizeof(Treasure)) == sizeof(Treasure)){
+    if (t.treasure_id != id) {
+      write(f_temp, &t, sizeof(Treasure));
+  } else {
+      found = 1;
+  }
+}
+  close(file);
+  close(f_temp);
+  
+  if(found==0){
+
+    printf("Comoara nu a fost gasita\n");
+    remove("temp.bin");
+    return;
+  }
+
+  rename("temp.bin",f_input);
+  char msg[128];
+  snprintf(msg,sizeof(msg),"Remove trasure %d", id);
+  log_opperation(hunt_id,msg);
+
+
+}
+
+void stergere_hunt(const char *hunt_id){
+
+  char f_input[FILENAME_SIZE];
+  snprintf(f_input,sizeof(f_input),"%s,%s",hunt_id,"treasures.bin");
+  remove(f_input);
+  
+  snprintf(f_input,sizeof(f_input),"%s,%s",hunt_id,FILE_INPUT);
+  remove(f_input);
+
+  rmdir(hunt_id);
+
+  char msg[128];
+  snprintf(msg,sizeof(msg),"Remove hunt %s",hunt_id);
+  log_opperation(hunt_id,msg);
 }
 
 int main(int argc,char **argv){
@@ -164,6 +227,10 @@ int main(int argc,char **argv){
     lista_treasure(hunt_id);
   }else if(strcmp(command,"--view") == 0 && argc == 4){
     view_treasure(hunt_id,atoi(argv[3]));
+  }else if(strcmp(command,"--remove_treasure") == 0 && argc == 4){
+    sterge_treasure(hunt_id,atoi(argv[3]));
+  }else if(strcmp(command,"--remove_hunt") == 0){
+    stergere_hunt(hunt_id);
   }
   else{
     perror("Eroare la comanda data \n");
