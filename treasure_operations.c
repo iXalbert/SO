d9bdeb0755c4d37d9dfe6b771742c8460f63b961
Rosca Albert <roscaalbert123@gmail.com>
2025-04-13 13:50:36 +0300

    Asa am trimis proiectul

--- treasure_operations.c
+++ treasure_operations.c
@@ -32,14 +32,34 @@ void log_opperation(const char *hunt_id,const char *message)
     write(file,log,strlen(log));
     close(file);
 
+    printf("Operatia indeplinita cu succes \n ");
+
     char name[FILENAME_SIZE];
     snprintf(name,sizeof(name),"logged_hunt-%s", hunt_id);
-    symlink(path,name);
+    if(symlink(path,name) < 0 )
+    {
+        perror("Eroare la creerea legaturi \n");
+        exit(-1);
+    }
+
 
 }
  
+int file_exist(const char *file)
+{
+    struct stat buffer;
+    return (stat(file, &buffer) == 0);
+}
+
 void adauga_comoara(const char *file_input, const char *hunt_id)
 {
+
+    if(!(file_exist(file_input)))
+    {
+        perror("Fisierul nu exista \n");
+        exit(-1);  
+    }
+
 
   mkdir(hunt_id, 0755);
 
@@ -64,7 +84,6 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
 
     char buffer[1024];
     ssize_t buffer_read = read(f_input,buffer,sizeof(buffer)-1);
-    close(f_input);
 
 
     if(buffer_read < 0)
@@ -82,6 +101,8 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     char *line = strtok(buffer,"\n"); 
     if(!line)
     {
+      perror("Format invalid 1 \n");
+      close(file);
       return;
     }
     t.treasure_id = atoi(line);
@@ -89,6 +110,8 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     line = strtok(buffer,"\n");
     if(!line)
     {
+      perror("Format invalid 2 \n");
+      close(file);
       return;
     }
     strncpy(t.username,line,USERNAME_SIZE);
@@ -97,6 +120,8 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     line = strtok(buffer,"\n");
     if(!line)
     {
+      perror("Format invalid 3 \n");
+      close(file);
       return;
     }
     t.longitude = atof(line);
@@ -104,6 +129,8 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     line = strtok(buffer,"\n");
     if(!line)
     {
+      perror("Format invalid 4 \n");
+      close(file);
       return;
     }
     t.latitude = atof(line);
@@ -111,6 +138,8 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     line = strtok(buffer,"\n");
     if(!line)
     {
+      perror("Format invalid 5 \n");
+      close(file);
       return;
     }
     strncpy(t.clue,line,USERNAME_SIZE);
@@ -119,6 +148,8 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     line = strtok(buffer,"/n");
     if(!line)
     {
+      perror("Format invalid 6 \n");
+      close(file);
       return;
     }
     t.value = atoi(line);
@@ -140,6 +171,7 @@ void adauga_comoara(const char *file_input, const char *hunt_id)
     close(file);
 
     char msg[128];
+
     snprintf(msg,sizeof(msg),"Treasure %d adaugata cu succes de user %s",t.treasure_id,t.username);
     log_opperation(hunt_id,msg);
 
@@ -151,6 +183,12 @@ void lista_treasure(const char *hunt_id)
   char f_input[FILENAME_SIZE];
   snprintf(f_input,sizeof(f_input),"%s/%s",hunt_id,"treasures.bin");
 
+  if(!(file_exist(f_input)))
+  {
+      perror("Fisierul nu exista \n");
+      exit(-1);  
+  }
+
   int file = open(f_input, O_RDONLY);
 
   if(file < 0 )
@@ -161,14 +199,16 @@ void lista_treasure(const char *hunt_id)
 
 
   struct stat st;
+
   if(stat(f_input,&st) == 0)
   {
-  printf("Hunt : %s\n", hunt_id);
-  printf("File size : %lld \n", st.st_size);
-  printf("Last modified : %s", ctime(&st.st_mtime));
+    printf("Hunt : %s\n", hunt_id);
+    printf("File size : %lld \n", st.st_size);
+    printf("Last modified : %s", ctime(&st.st_mtime));
   }
 
   Treasure t;
+
   while (read(file,&t,sizeof(Treasure)) == sizeof(Treasure))
   { 
     printf("ID : %d, User : %s, Lat : %.2f, Lon : %.2f, Value : %d, Clue : %s \n", t.treasure_id, t.username, t.latitude, t.longitude, t.value, t.clue);
@@ -184,6 +224,12 @@ void view_treasure(const char *hunt_id,int id)
   char f_input[FILENAME_SIZE];
   snprintf(f_input,sizeof(f_input),"%s/%s",hunt_id,"treasures.bin"); 
 
+  if(!(file_exist(f_input)))
+  {
+      perror("Fisierul nu exista \n");
+      exit(-1);  
+  }
+
   int file = open(f_input, O_RDONLY);
 
   if(file < 0 )
@@ -193,6 +239,7 @@ void view_treasure(const char *hunt_id,int id)
   }  
 
  Treasure t;
+
   while (read(file,&t,sizeof(Treasure)) == sizeof(Treasure))
   { 
     if(t.treasure_id == id)
@@ -200,6 +247,7 @@ void view_treasure(const char *hunt_id,int id)
     printf("ID : %d, User : %s, Lat : %.2f, Lon : %.2f, Value : %d, Clue : %s \n", t.treasure_id, t.username, t.latitude, t.longitude, t.value, t.clue);
 
     char msg[128];
+
     snprintf(msg,sizeof(msg), "Treasure gaist %d", id);
     log_opperation(hunt_id,msg);
     close(file);
@@ -216,7 +264,14 @@ void sterge_treasure(const char *hunt_id, int id)
   char f_input[FILENAME_SIZE];
   snprintf(f_input,sizeof(f_input),"%s/%s",hunt_id,"treasures.bin");
 
+  if(!(file_exist(f_input)))
+  {
+      perror("Fisierul nu exista \n");
+      exit(-1);  
+  }
+
   int file = open(f_input, O_RDONLY);
+
   if (file < 0)
   {
     perror("Eroare la deschiderea fisierului de stergere");
@@ -224,6 +279,7 @@ void sterge_treasure(const char *hunt_id, int id)
   }
 
   int f_temp = open("temp.bin", O_WRONLY| O_CREAT | O_TRUNC, 0644);
+
   if(f_temp < 0)
   {
     perror("Eroare la deschiderea fisierului temporar in stergere");
@@ -233,6 +289,7 @@ void sterge_treasure(const char *hunt_id, int id)
 
   Treasure t;
   int found = 0;
+
   while (read(file,&t,sizeof(Treasure)) == sizeof(Treasure))
   {
     if (t.treasure_id != id) 
@@ -244,6 +301,7 @@ void sterge_treasure(const char *hunt_id, int id)
       found = 1;
     }
 }
+
   close(file);
   close(f_temp);
   
@@ -256,7 +314,9 @@ void sterge_treasure(const char *hunt_id, int id)
   }
 
   rename("temp.bin",f_input);
+
   char msg[128];
+
   snprintf(msg,sizeof(msg),"Remove trasure %d", id);
   log_opperation(hunt_id,msg);
 
@@ -267,6 +327,7 @@ void sterge_hunt(const char *hunt_id)
 {
 
   char f_input[FILENAME_SIZE];
+
   snprintf(f_input,sizeof(f_input),"%s/%s",hunt_id,"treasures.bin");
   remove(f_input);
   
@@ -276,6 +337,7 @@ void sterge_hunt(const char *hunt_id)
   rmdir(hunt_id);
 
   char name_symlink[FILENAME_SIZE];
+
   snprintf(name_symlink,sizeof(name_symlink),"logged_hunt-%s", hunt_id);
   unlink(name_symlink);
 
